c33aa87aad58257758dce9a15093fc149d7d72f3
Azamat <azamt14@gmail.com>
2021-12-07 20:35:57 +0300

    72.js add

---
+++ 72.js
@@ -0,0 +1,24 @@
+function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+      case "a":
+        answer = "apple"
+        return answer
+        break
+      case "b":
+        answer = "bird"
+        return answer
+        break
+      case "c":
+        answer = "cat"
+        return answer
+        break
+      default:
+        return "stuff"
+        break
+    }
+  
+  }
+  
+  switchOfStuff(1);
