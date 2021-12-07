1371b91a04ca1639e1504d73a06522bfa63300ad
Azamat <azamt14@gmail.com>
2021-12-07 20:20:33 +0300

    71.js

---
+++ 71.js
@@ -0,0 +1,26 @@
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val) {
+      case 1:
+        answer = "alpha"
+        return answer
+        break
+       case 2:
+        answer = "beta"
+        return answer
+        break
+       case 3:
+        answer = "gamma"
+        return answer
+        break
+       case 4:
+        answer = "delta"
+        return answer
+        break
+    }
+    // nly change code above this line
+    return answer;
+  }
+  
+  caseInSwitch(1);
