b8ab72bcff7ee51a5a556e3b1e391389d27c6b31
Azamat <azamt14@gmail.com>
2021-12-15 12:19:09 +0300

    complex objects

---
+++ 88.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Nirvana",
+      "title": "Nevermind",
+      "release_year": 1991,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
